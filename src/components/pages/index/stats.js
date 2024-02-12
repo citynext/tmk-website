a0bf48ea63e30d367ef993760bdcafb590bb90b7
Amine SGHIR <mp6099@engie.com>
2024-02-12 02:42:53 +0100

    Refactor stats component layout and styles

--- src/components/pages/index/stats.js
+++ src/components/pages/index/stats.js
@@ -9,23 +9,26 @@ const StatItem = ({icon, kpi, text}) => {
         <div className="flex flex-col justify-end items-center">
             <div className="flex items-center mb-1">
                 <_icon className="text-secondary text-5xl" />
-                <span className="text-white text-6xl font-bold italic ml-3">{kpi}</span>
+                <span className="text-white text-3xl sm:text-6xl font-bold italic ml-3">{kpi}</span>
             </div>
-            <label className="text-white text-2xl font-light">{text}</label>
+            <label className="text-white text-center text-xl sm:text-2xl font-light">{text}</label>
         </div>
     )
 }
 
 
 export default function Stats() {
+    const className = {
+        li: 'flex-1 scale-75 md:scale-100',
+    }
     return (
         <section className="relative">
             <img src="images/general-landing/stats-bg.svg" className="w-screen h-48 object-cover" />
-            <div className="absolute top-0 px-20 left-0 w-full h-full flex justify-center">
+            <div className="absolute top-0 px-0 md:px-20 left-0 w-full h-full flex justify-center">
                 <ul className="flex justify-around items-center w-full max-w-[80rem]">
-                    <li><StatItem icon={BiSolidHappyHeartEyes} kpi="97%" text="Satisfaction client" /></li>
-                    <li><StatItem icon={BsPeopleFill} kpi="200+" text="Membres" /></li>
-                    <li><StatItem icon={FaHeartCirclePlus} kpi="0" text="Stress" /></li>
+                    <li className={className.li}><StatItem icon={BiSolidHappyHeartEyes} kpi="97%" text="Satisfaction client" /></li>
+                    <li className={className.li}><StatItem icon={BsPeopleFill} kpi="200+" text="Membres" /></li>
+                    <li className={className.li}><StatItem icon={FaHeartCirclePlus} kpi="0" text="Stress" /></li>
                 </ul>
             </div>
         </section>
