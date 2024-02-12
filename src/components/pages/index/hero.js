8595f9774147b45033a678c2af14d89b89452b05
Amine SGHIR <mp6099@engie.com>
2024-02-12 02:08:20 +0100

    Refactor CTA component and update hero styles

--- src/components/pages/index/hero.js
+++ src/components/pages/index/hero.js
@@ -21,19 +21,20 @@ const Input = (props) => {
     );
 }
 
-function CTA({ className, ...props }) {
+function CTA({ className }) {
+    console.log(className)
     return (
         <section className={className}>
-            <div className="bg-white w-fit px-5 py-2 font-medium text-lg rounded-tl-2xl rounded-tr-2xl border-gray-400 border-l border-t border-r">Calculez votre revenu garanti</div>
-            <div className="bg-white h-fit flex justify-between items-center rounded-2xl rounded-tl-none border-gray-400 border-solid border-b border-x p-5">
+            <div className="bg-white opacity-80 lg:opacity-100 w-fit px-5 py-2 font-medium text-lg rounded-tl-2xl rounded-tr-2xl border-gray-400 border-l border-t border-r">Calculez votre revenu garanti</div>
+            <div className="bg-white opacity-80 flex-wrap lg:opacity-100 h-fit flex justify-between items-center rounded-2xl rounded-tl-none border-gray-400 border-solid border-b border-x p-5">
                 <Input type="text" icon={IoLocationOutline} label="Localisation" placeholder="Où habitez-vous ?" className="w-80" />
                 {/* <div className="border-l border-gray-400 h-20 mx-5"></div> */}
-                <Input type="number" icon={LiaDoorOpenSolid} label="Chambre(s)" placeholder="0" className="w-48 border-gray-400 border-solid border-l border-r" />
+                <Input type="number" icon={LiaDoorOpenSolid} label="Chambre(s)" placeholder="0" className="w-48 border-gray-400 lg:border-solid lg:border-l lg:border-r" />
                 {/* <div className="border-l border-gray-400 h-20 mx-5"></div> */}
                 <Input type="number" icon={IoPersonOutline} label="Capacité d'accueil" placeholder="0" className="w-56" />
                 <div className="flex flex-col">
                     <button className="bg-secondary text-white px-5 py-1 rounded-3xl ">Calculer</button>
-                    <button className="rounded-3xl border border-text mt-2">Voir plus</button>
+                    {/* <button className="rounded-3xl border border-text mt-2">Voir plus</button> */}
                 </div>
             </div>
         </section>
@@ -44,11 +45,11 @@ export default function Hero() {
     return (
     <div className="relative flex justify-center h-screen">
         <img src="/images/general-landing/hero.jpeg" alt="Image principale" className="h-[95vh] w-screen object-cover" />
-        <div className="absolute flex flex-col justify-center inset-0 bg-gradient-to-b from-primary to-transparent px-20 text-white">
-          <h1>TAKE MY KEYS te permettra <br />de <strong className="bg-secondary px-2 pb-2">voyager</strong></h1>
+        <div className="absolute flex flex-col justify-end lg:justify-center inset-0 bg-gradient-to-b from-primary to-transparent px-5 lg:px-20 text-white">
+          <h1 className="text-4xl leading-[3rem] lg:leading-[6rem] lg:text-[4.5rem]">TAKE MY KEYS te permettra <br />de <strong className="bg-secondary px-2 pb-2">voyager</strong></h1>
           <h2 className="text-2xl font-light">Tout en payant ton loyer durant ton absence... et bien plus encore</h2>
+          <CTA className="text-text my-20 lg:my-0 w-fit lg:mx-auto lg:left-0 lg:right-0 lg:absolute lg:bottom-0 lg:h-36" />
         </div>
-        <CTA className="bg-transparent absolute bottom-0 h-36" />
       </div>
     )
 }
