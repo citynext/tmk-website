9320c2bee5bf69cb01e034d3517f512614eaf4df
Amine SGHIR <mp6099@engie.com>
2024-02-13 14:32:34 +0100

    Add conciergerie services data and update services component

--- src/components/pages/conciergerie/services.js
+++ src/components/pages/conciergerie/services.js
@@ -1,20 +1,22 @@
 import Image from 'next/image'
 import { HiOutlineArrowSmallLeft, HiOutlineArrowSmallRight } from 'react-icons/hi2'
+import services from '@/data/conciergerie/services.json'
 
 
-const Service = ({image, title, description}) => {
+const Service = ({ image, title, description }) => {
     return (
-        <div className="flex flex-col items-center mx-2">
-            <Image src={image} alt={title} width={400} height={100} />
-            <h3 className='text-md my-4'>{title}</h3>
+        <div className="flex flex-col justify-between mx-2 flex-1">
+            <Image src={image} alt={title} width={400} height={400} />
+            <h3 className='text-md my-4 uppercase'>{title}</h3>
             <p className="!text-sm">{description}</p>
         </div>
     )
 }
 
 
-export default function Services() {
 
+
+export default function Services() {
     const nextStep = () => {
         console.log("next step")
     }
@@ -27,10 +29,11 @@ export default function Services() {
         <section className="flex flex-col items-center my-10 py-10 px-20 bg-third">
             <h1>Nos services</h1>
             <p>Découvrez nos services - Votre tranquilité, Notre signature</p>
-            <div className='flex items-center mt-16 text-center'>
-                <Service image="/images/conciergerie/phone.svg" title="MAINTENANCE ET ENTRETIEN" description="Lorem ipsum" />
-                <Service image="/images/conciergerie/phone.svg" title="MAINTENANCE ET ENTRETIEN" description="Lorem ipsum" />
-                <Service image="/images/conciergerie/phone.svg" title="MAINTENANCE ET ENTRETIEN" description="Lorem ipsum" />
+            <div className='flex mt-16 text-center'>
+                {services.map((service, index) => (
+                    <Service key={index} image={service.image} title={service.title} description={service.description} />
+                ))
+                }
             </div>
             <div className="flex justify-center items-center mt-5">
                 <button className={`text-3xl bg-text text-white rounded-full p-3 m-4`} onClick={prevStep}><HiOutlineArrowSmallLeft /></button>
