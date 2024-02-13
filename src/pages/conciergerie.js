3afbf533677045a9bb264adaa04bff00d8afac70
Amine SGHIR <mp6099@engie.com>
2024-02-13 16:31:57 +0100

    Add Process component to ConciergeriePage

--- src/pages/conciergerie.js
+++ src/pages/conciergerie.js
@@ -2,6 +2,7 @@ import styles from '../styles/pages/conciergerie.module.css'
 import Hero from '../components/pages/conciergerie/hero';
 import Why from '../components/pages/conciergerie/why';
 import Services from '../components/pages/conciergerie/services';
+import Process from '../components/pages/conciergerie/process';
 
 
 export default function ConciergeriePage() {
@@ -10,6 +11,7 @@ export default function ConciergeriePage() {
             <Hero />
             <Why />
             <Services />
+            <Process />
         </main>
     )
 }
