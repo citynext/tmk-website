5d245070a1c10896c056990e6b141f3aada1a630
Amine SGHIR <mp6099@engie.com>
2024-03-17 14:35:06 +0100

    Refactor clientReviews component to use Embla Carousel

--- src/components/pages/conciergerie/clientReviews.js
+++ src/components/pages/conciergerie/clientReviews.js
@@ -1,39 +1,50 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import data from "../../../data/conciergerie/clientReviews.json";
-import { FaStar } from "react-icons/fa";
-import Carousel from "@/components/reusable/carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from 'embla-carousel-autoplay'
+import { FaStar } from "react-icons/fa6";
+import reviews from "@/data/conciergerie/clientReviews.json";
+import anonymous from "@/../public/images/anonymous.jpeg";
 
 export default function ClientReviews() {
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({}, [Autoplay()]);
+  const [selected , setSelected] = useState(0);
+
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.on("select", () => {
+        setSelected(emblaApi.selectedScrollSnap());
+      });
+    }
+  }, [emblaApi])
   return (
     <section className="flex flex-col justify-center items-center px-10">
-      <h1 className="text-center">Ce que nos clients disent de nous</h1>
-      <Carousel className="w-fit mt-32" autoplay options={{ loop: true }}>
-        {data.map((review, index) => (
-          <div
+      <h1 className="text-center !mb-10">Ce que nos clients disent de nous</h1>
+      <div className="w-screen sm:w-auto max-w-full" ref={emblaRef}>
+        <ul className="w-full flex mb-10 sm:mb-0 mt-24 gap-4 sm:gap-24">
+          {reviews.map((review, index) => (
+            <li
             key={index}
-            className="relative bg-white shadow-2xl rounded-2xl mx-6 p-4 w-72 sm:w-96"
-          >
-            <div className="absolute flex justify-center items-center -top-20 left-0 right-0 w-40 h-40 mx-auto bg-primary rounded-full z-[-1]"></div>
-            <div className="absolute -top-16 left-0 right-0 mx-auto w-32 h-32 z-20">
-              <Image
-                src={review.image}
-                alt={review.name}
-                fill
-                className="object-cover rounded-full"
-              />
-            </div>
-            <div className="mt-16 flex flex-col justify-between items-center text-center">
-              <h2 className="">{review.name}</h2>
-              <p className="!text-base">{review.review}</p>
-              <div className="my-4 w-fit grid grid-cols-5 gap-2 text-yellow-400">
-                {Array.apply(null, Array(5)).map((v, i) => (
-                  <FaStar key={i} />
-                ))}
+            className="relative rounded-lg bg-white p-8 shadow-2xl min-w-full sm:min-w-96 w-full sm:w-96 pt-20"
+            >
+              <div className="absolute -top-20 left-0 right-0 mx-auto bg-primary p-2 h-20 w-40 -z-10" style={{borderRadius: "10rem 10rem 0 0"}}></div>
+              <Image src={anonymous} alt={review.name} className="absolute -top-16 left-0 right-0 mx-auto size-32 object-cover rounded-full z-10" />
+              <div className="flex flex-col justify-between items-center text-center gap-6">
+                <h2 className="text-center">{review.name}</h2>
+                <p className="bg-white !text-base text-center line-clamp-[8] z-0">
+                  {review.review}
+                </p>
+                <div className="my-4 w-fit grid grid-cols-5 gap-2 text-yellow-400">
+                  {Array.apply(null, Array(5)).map((v, i) => (
+                    <FaStar key={i} />
+                  ))}
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
-      </Carousel>
+          </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
