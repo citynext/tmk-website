bd3a03a730b5e226fb88e507386b8ca0edff3cb8
Amine SGHIR <mp6099@engie.com>
2024-03-20 03:54:16 +0100

    Update Google Analytics tracking code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,16 +6,16 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script id="google-analytics">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-K5DZ3ZP2');`}</Script>
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}</Script>
       </Head>
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K5DZ3ZP2"
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
