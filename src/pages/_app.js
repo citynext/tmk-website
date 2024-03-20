645d357a8aa3aa8f51a8c4c22658a6790ac20aea
Amine SGHIR <mp6099@engie.com>
2024-03-20 13:09:52 +0100

    Remove posthog-js dependency and update Google Analytics integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,6 @@
 import DefaultLayout from "../components/layout"
 import "../styles/globals.css"
-import posthog from "posthog-js"
-import { PostHogProvider } from 'posthog-js/react'
-
-if (typeof window !== 'undefined') { // checks that we are client-side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
-    },
-  })
-}
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 export default function App({ Component, pageProps }) {
@@ -18,9 +8,8 @@ export default function App({ Component, pageProps }) {
 
   return getLayout(
     <>
-      <PostHogProvider client={posthog}>
-        <Component {...pageProps} />
-      </PostHogProvider>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
     </>
   );
 }
